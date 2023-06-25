9bc4de8c595c95a4f84871d7faa2b28643f5d11f
Serdal58 <sserdalyildirim58@gmail.com>
2023-06-25 23:31:21 +0300

    c# ile bılgı yarısması oyunu

---
+++ BılgıYYarısmasıı/BılgıYYarısmasıı/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace BılgıYYarısmasıı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lblsoru = new System.Windows.Forms.Label();
+            this.lblpuhan = new System.Windows.Forms.Label();
+            this.lblpuan = new System.Windows.Forms.Label();
+            this.lblzaman = new System.Windows.Forms.Label();
+            this.lblsure = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(40, 306);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(244, 306);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 58);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(632, 23);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 58);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Başla";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(1, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(439, 140);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(629, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Doğru";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(629, 167);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(42, 17);
+            this.lbl.TabIndex = 7;
+            this.lbl.Text = "Soru:";
+            // 
+            // lblsoru
+            // 
+            this.lblsoru.AutoSize = true;
+            this.lblsoru.Location = new System.Drawing.Point(680, 167);
+            this.lblsoru.Name = "lblsoru";
+            this.lblsoru.Size = new System.Drawing.Size(16, 17);
+            this.lblsoru.TabIndex = 8;
+            this.lblsoru.Text = "0";
+            // 
+            // lblpuhan
+            // 
+            this.lblpuhan.AutoSize = true;
+            this.lblpuhan.Location = new System.Drawing.Point(680, 123);
+            this.lblpuhan.Name = "lblpuhan";
+            this.lblpuhan.Size = new System.Drawing.Size(16, 17);
+            this.lblpuhan.TabIndex = 10;
+            this.lblpuhan.Text = "0";
+            // 
+            // lblpuan
+            // 
+            this.lblpuan.AutoSize = true;
+            this.lblpuan.Location = new System.Drawing.Point(629, 123);
+            this.lblpuan.Name = "lblpuan";
+            this.lblpuan.Size = new System.Drawing.Size(45, 17);
+            this.lblpuan.TabIndex = 9;
+            this.lblpuan.Text = "Puan:";
+            // 
+            // lblzaman
+            // 
+            this.lblzaman.AutoSize = true;
+            this.lblzaman.Location = new System.Drawing.Point(671, 217);
+            this.lblzaman.Name = "lblzaman";
+            this.lblzaman.Size = new System.Drawing.Size(16, 17);
+            this.lblzaman.TabIndex = 12;
+            this.lblzaman.Text = "0";
+            // 
+            // lblsure
+            // 
+            this.lblsure.AutoSize = true;
+            this.lblsure.Location = new System.Drawing.Point(620, 217);
+            this.lblsure.Name = "lblsure";
+            this.lblsure.Size = new System.Drawing.Size(42, 17);
+            this.lblsure.TabIndex = 11;
+            this.lblsure.Text = "Süre:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblzaman);
+            this.Controls.Add(this.lblsure);
+            this.Controls.Add(this.lblpuhan);
+            this.Controls.Add(this.lblpuan);
+            this.Controls.Add(this.lblsoru);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Bilgi yarışması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblsoru;
+        private System.Windows.Forms.Label lblpuhan;
+        private System.Windows.Forms.Label lblpuan;
+        private System.Windows.Forms.Label lblzaman;
+        private System.Windows.Forms.Label lblsure;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BılgıYYarısmasıı/BılgıYYarısmasıı/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BılgıYYarısmasıı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        SqlConnection baglan = new SqlConnection("Data Source=LAPTOP-SG7L1Q97\\SQLEXPRESS01;Initial Catalog=Sorularrr;Integrated Security=True");
+        int sayac = 0;
+        int puan = 0;
+        int zaman = 20;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            zaman = 21;
+
+
+            button5.Text="Sonraki";
+            sayac++;
+            lblsoru.Text = sayac.ToString();
+            button5.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+            if (sayac==1)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular1 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+               
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+
+                }
+                
+               
+            }
+            baglan.Close();
+            if (sayac==2)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular2 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+
+                }
+               
+            }
+            baglan.Close();
+            if (sayac==3)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular3 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+
+                }
+                baglan.Close();
+            }
+            if (sayac==4)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular3 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+
+                }
+                baglan.Close();
+            }
+            if (sayac==5)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular4 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+                }
+            }
+            if (sayac==6)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular5 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+                }
+            }
+            if (sayac==7)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular5 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+                }
+            }
+            if (sayac==8)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular6 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+                }
+            }
+            if (sayac==9)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular7 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+                }
+            }
+            if (sayac==10)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select*from Sorular7 order by NEWID()", baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    button1.Text = (oku["a"].ToString());
+                    button2.Text = (oku["b"].ToString());
+                    button3.Text = (oku["c"].ToString());
+                    button4.Text = (oku["d"].ToString());
+                    textBox1.Text = (oku["soru"].ToString());
+                    label1.Text = (oku["dogru"].ToString());
+                   
+
+                }
+            }
+            if (sayac==11)
+            {
+                MessageBox.Show("Oyun bitti");
+                timer1.Enabled = false;
+            }
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblsoru.Text = sayac.ToString();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (button1.Text==label1.Text)
+            {
+                
+                puan = puan + 10;
+                lblpuhan.Text = puan.ToString();
+
+
+
+
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text==label1.Text)
+            {
+                puan = puan + 10;
+                lblpuhan.Text = puan.ToString();
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text==label1.Text)
+            {
+
+                puan = puan + 10;
+                lblpuhan.Text = puan.ToString();
+
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text==label1.Text)
+            {
+                puan = puan + 10;
+                lblpuhan.Text = puan.ToString();
+
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zaman = zaman - 1;
+            lblzaman.Text = zaman.ToString();
+            if (zaman==0)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                timer1.Enabled = false;
+                button5.Enabled = true;
+
+
+            }
+
+        }
+    }
+}
